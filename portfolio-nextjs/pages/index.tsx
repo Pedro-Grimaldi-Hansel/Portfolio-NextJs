29216f4fd8f486b716b1b948dc5ffb9e7229c8d0
Pedro Grimaldi <pedroghansel@gmail.com>
2022-11-01 18:26:57 -0300

    parte de cima foita da home mas os icones tão começando agr

--- portfolio-nextjs/pages/index.tsx
+++ portfolio-nextjs/pages/index.tsx
@@ -2,17 +2,36 @@ import type { NextPage } from 'next'
 import Navbar from '../components/NavBar'
 import styles from '../styles/index.module.scss'
 import Image from "next/image";
-import foto from '../styles/Images/Pedro.jpg'
+import foto from '../public/Images/Pedro.jpg'
+import foto2 from '../public/Images/git-original.svg'
 
 const Home: NextPage = () => {
   return (
     <> 
       <Navbar/>
       <hr/>
-      <section className={styles.Home}>
-      <div className={styles.foto_de_perfil}>
+      <section className={styles.Home_top}>
+                <div className={styles.foto_de_perfil}>
                     <a href="https://github.com/Pedro-Grimaldi-Hansel" target="_blank"><Image src={foto}/></a>
                 </div>
+
+                <div className={styles.Texto_de_introducao}>
+                <h1>Pedro <span>Grimaldi Hansel</span></h1>
+                    <h2>Estudante de Graduação em <a href="https://www2.ufjf.br/ufjf/ensino/graduacao/ciencias-exatas/" target="_blank"
+                    >Ciencias Exatas</a> e <a href="https://www.ufjf.br/cursocomputacao/" target="_blank"
+                    >Ciencias da Computação</a> (UFJF).</h2>
+                    <h2>Projetista, acessor do departamento comercial e departamentos projetos na  
+                    <a href="https://www.instagram.com/codejr/"target="_blank"> Code Empresa Jr de computação.</a></h2>
+                </div>
+      </section>
+      <section className={styles.Home_bottom}>
+
+        <div className={styles.icons_container}>
+            
+          <Image src={foto2}/>
+            
+        </div>
+
       </section>
     </>
     
