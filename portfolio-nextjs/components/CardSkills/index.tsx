e48f77edcfe39dea9e6850df7402260316b99625
Pedro Grimaldi <pedroghansel@gmail.com>
2022-12-11 22:55:15 -0300

    pagina skills funcional e responsiva porem da p ficar mais bonita

--- portfolio-nextjs/components/CardSkills/index.tsx
+++ portfolio-nextjs/components/CardSkills/index.tsx
@@ -5,14 +5,20 @@ import styles from "./CardSkills.module.scss";
 
 interface CardSkillsProps{
     skill: string;
-    //icones: string;
+    icones: string;
     texto: string;
 }
 
 const CardSkills = (props:CardSkillsProps) => {
   return (
     <section className={styles.CardSkills}>
-      <div className={styles.Card}>  
+      
+      <div className={styles.Card}>
+        <div className={styles.Icone}>
+          <div className={styles.Img}>
+            <img src={props.icones} alt="Icone da skill" /> 
+          </div>
+        </div> 
         <div className={styles.Titulo}>
           <h1>{props.skill}</h1>
         </div>
--- portfolio-nextjs/pages/Skills.tsx
+++ portfolio-nextjs/pages/Skills.tsx
@@ -12,44 +12,58 @@ const Skills = () => {
         <section className={styles.Skills}>
           
           <div className={styles.Cards}>
-            <CardSkills skill='HTML / CSS' 
-            texto='Capacidade de desenvolver projetos Front-end usando as linguagens HTML e CSS e uso de frameworks como por exemplo Bootstrap.'
+            <CardSkills skill='HTML' 
+            icones= './Images/html5-original.svg'
+            texto='Base solida em HTML e capacidade de usar a linguagem nos desenvolvimentos Front-End e Back-End.'
+            />
+          </div> 
+
+          <div className={styles.Cards}>
+            <CardSkills skill='CSS' 
+            icones= './Images/CSS3-original.svg'
+            texto='Base solida em CSS e SASS, permitindo alta facilidade na ciração de sites responsivos e estilizados.'
             />
           </div> 
 
           <div className={styles.Cards}>
             <CardSkills skill='JavaScript' 
+            icones= './Images/javascript-original.svg'
             texto='Base solida em JavaScript e capacidade de usar a linguagem nos desenvolvimentos Front-End e Back-End.'
             />
           </div>
           <div className={styles.Cards}>
             <CardSkills skill='ReactJs' 
+            icones= './Images/react-original-wordmark.svg'
             texto="Com a biblioteca ReactJs posso programar o Front-end completo de projetos web e promover a interação com o Back-end integrando API's."
             />
           </div>
 
           <div className={styles.Cards}>  
             <CardSkills skill='NextJs' 
+            icones= './Images/nextjs-original-wordmark.svg'
             texto='Com NextJs posso programar o Front-end completo de projetos web (como esse site por exemplo) e promover a interação com o Back-end.'
             />
           </div>  
 
           <div className={styles.Cards}>
             <CardSkills skill='NodeJs' 
-            texto="Capacidade de desenvolver Api's Rest para o Back-end usando a biblioteca NodeJs, para a interação do Front-End com o banco de dados 
-            (MongoDB ou outros) por meio de CRUD's, interações especificas, restrições de acessos e etc."
+            icones= './Images/nodejs-original.svg'
+            texto="Posso desenvolver API's Rest, para a interação do Front-End com o banco de dados 
+            por CRUD's, interações especificas, restrições de acessos e etc."
             />
           </div>
 
           <div className={styles.Cards}>
             <CardSkills skill='C / C++' 
-            texto='Durante a faculdade diversas materias que envolvem programação (algoritimos, estruturas de dados 1 e 2, teoria dos
+            icones= './Images/cplusplus-original.svg'
+            texto='Durante a faculdade diversas materias (algoritimos, estruturas de dados 1 e 2, teoria dos
               grafos e outras) são lecionadas em C e C++.'
             />
           </div>
 
           <div className={styles.Cards}>
             <CardSkills skill='Git, Github e Bitbucket' 
+            icones= './Images/git-original.svg'
             texto='Trabalho com o metodo de branchs e tenho noção dessas ferramentas para o versionamento de códigos.'
             />
           </div>
