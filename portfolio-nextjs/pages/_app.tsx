199e0957be235535c9c87f19452152c1f65e25ee
Pedro Grimaldi <pedroghansel@gmail.com>
2022-12-20 01:03:35 -0300

    atualizando o next pra 13

--- portfolio-nextjs/pages/_app.tsx
+++ portfolio-nextjs/pages/_app.tsx
@@ -1,5 +1,12 @@
 import '../styles/globals.scss'
+import { Montserrat } from '@next/font/google'
 import type { AppProps } from 'next/app'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: '400'
+})
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
